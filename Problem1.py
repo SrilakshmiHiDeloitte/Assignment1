3d1658b5a9c2e1c534cf17d34b990f1c2c58a7fa
visrilakshmi <visrilakshmi@deloitte.com>
2022-02-21 18:46:24 +0530

    first assignment

---
+++ Problem1.py
@@ -0,0 +1,59 @@
+class StringClass:
+    def __init__(self, userinput):
+        self.userinput = userinput
+
+    def stringlength(self):
+        print(len(self.userinput))
+
+    def stringtolist(self):
+        print(list(self.userinput))
+
+
+class PairsPossible(StringClass):
+    pairs = []
+
+    def __init__(self, userinput):
+        StringClass.__init__(self, userinput)
+
+    def getpairs(self):
+        k = self.userinput
+        a = len(self.userinput)
+        op = []
+        for i in range(0, a):
+            for j in range(1, a):
+                b = ''
+                if len(b) <= 2:
+                    b = b + str(k[i]) + str(k[j])
+                    op.append(b)
+        print(op)
+
+    """def getpairswidspace(self):
+        for i in self.getpairs():
+            print(i, end=' ')"""
+
+
+class SearchCommonElements(StringClass):
+    def __init__(self, userinput, userinput2):
+        self.userinput2 = userinput2
+        StringClass.__init__(self, userinput)
+
+    def comelements(self):
+        k = self.userinput
+        j = self.userinput2
+        c = 0
+        d = {}
+        for i in k:
+            c = 0
+            for j in j:
+                if i == j:
+                    c = c + 1
+            d[i] = c
+        print(d)
+
+
+class EqualSumPairs(SearchCommonElements):
+    def __init__(self):
+        print(SearchCommonElements.comelements())
+
+
+usr = EqualSumPairs('Srilakshmi', 'lak')
