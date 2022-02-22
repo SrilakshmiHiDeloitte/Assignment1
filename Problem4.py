ef589967d2da56a515eba4812e00fa891a27401c
visrilakshmi <visrilakshmi@deloitte.com>
2022-02-22 18:15:06 +0530

    second assigment

--- Problem4.py
+++ Problem4.py
@@ -1,13 +1,14 @@
-
 # Problem1
 import cmath
 
-x = lambda a, b, c: ((-b - cmath.sqrt(b ** 2 - (4 * a * c))) / (2 * a), (-b + cmath.sqrt(b ** 2 - (4 * a * c))) / (2 * a))
+x = lambda a, b, c: (
+(-b - cmath.sqrt(b ** 2 - (4 * a * c))) / (2 * a), (-b + cmath.sqrt(b ** 2 - (4 * a * c))) / (2 * a))
 print(x(2, 4, 1))
-
 
 # Problem2
 a = ["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
-x = sum(list(map(lambda b: (i for i in a if 'A' in i or 'a' in i), a)))
 
-print(x)
+
+y = list(map(lambda x: (a.count(x)) if 'A' in x or 'a' in x else x, a))
+
+print(y)
