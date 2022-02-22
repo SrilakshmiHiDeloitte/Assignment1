59cbcde73259333e7f8914413fb37e7405977b71
visrilakshmi <visrilakshmi@deloitte.com>
2022-02-22 12:41:25 +0530

    assignment solution

--- Problem3.py
+++ Problem3.py
@@ -30,18 +30,42 @@ def merge(a, b):
 
 merge(["Hello ", "take "], ["Dear", "Sir"])
 
-def appendlist(a,b):
-    for i in a:
-        for j in i:
-            for k in j:
-                for m in b:
-                    k.append(m)
-    print(k)
 
+def appendlist(a, b):
+    c = a[2][1][2]
+    for i in b:
+        c.append(i)
+    print(a)
 
 
 appendlist(["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"], ['h', 'i', 'j'])
 
 
+def mapping(a, b):
+    c = {i: j for i, j in zip(a, b)}
+    print(c)
 
 
+mapping(['Ten', 'Twenty', 'Thirty'], [10, 20, 30])
+
+
+def replacing(a, b):
+    for i in a.keys():
+        if i == 'city':
+            a[i] = b
+    print(a)
+
+
+replacing({"name": "Kelly", "age": 25, "salary": 8000, "city": "New york"}, 'Rajahmundry')
+
+
+def dictolist(a):
+    op = []
+    for i in a.keys():
+        m = a.get(i)
+        m.insert(0,i)
+        op.append(m)
+    print(op)
+
+
+dictolist({'HuEx': [1, 3, 4], 'is': [7, 6], 'best': [4, 5]})
