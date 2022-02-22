03df75b0642e1cc4570563b3011f88fe5fee5a05
visrilakshmi <visrilakshmi@deloitte.com>
2022-02-22 09:25:51 +0530

    first assignment

--- Problem2.py
+++ Problem2.py
@@ -1,44 +1,59 @@
 def pattern1(n):
-    """j = 1
-    for i in range(0, n):
-        k = i + j
-        print(k*"*")
-        j = j + 1"""
     m = 1
     for i in range(0, n):
-        k = ((n-i)*'  ')
-        l = (i+m)*'*'
-        m = m+1
+        k = ((n - i) * '  ')
+        l = (i + m) * '*'
+        m = m + 1
 
-        print(k,l)
-    m = n-1
-    for i in reversed(range(0, n-1)):
+        print(k, l)
+    m = n - 1
+    for i in reversed(range(0, n - 1)):
         k = ((n - i) * '  ')
         l = (i + m) * '*'
         m = m - 1
 
         print(k, l)
 
-        """for k in range(0, i + 2):
-            print("*", end="")"""
 
 def pattern2(n):
-    for i in range(n):
-        # printing spaces
-        for j in range(n - i - 1):
-            print(' ', end='')
-
-        # printing stars
-        for k in range(2 * i + 1):
-            # print star at start and end of the row
-            if k == 0 or k == 2 * i:
-                print('*', end='')
-            else:
-                if i == n - 1:
-                    print('*', end='')
-                else:
-                    print(' ', end='')
-        print(end='\n')
+    j = 0
+    for i in range(1, n + 1):
+        if i <= 2 or i == n:
+            k = ((n - i) * ' ')
+            print(k + i * '* ' + k)
+        else:
+            k = (n - i) * ' '
+            h = (i - 1 + j) * ' '
+            print(k + '* ' + h + '* ' + k)
+            j = j + 1
 
 
-pattern1(5)
+def pattern3(n):
+    for i in reversed(range(0, n + 1)):
+        if i <= 2 or i == n:
+            k = (n - i) * '  '
+            print(k + (i * '* '))
+        else:
+            k = (n - i) * '  '
+            l = (i - 2) * '  '
+            print(k + '* ' + l + '* ')
+
+
+def pascal(n):
+    j = 0
+    for i in range(1, n + 1):
+        if i <= 2:
+            k = (n-i)*' '
+            print(k+i*'1 ')
+        else:
+            k = (n-i) * ' '
+            print(k,end='')
+            for j in range(1,i):
+                print(str(j)+' ',end='')
+            print('1 ')
+
+
+
+
+
+pascal(5)
